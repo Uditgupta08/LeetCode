4e21c68932d8dddd62dd17a2bf618535f59f7001
Udit Gupta <135364043+Uditgupta08@users.noreply.github.com>
2025-07-10 17:51:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (75.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int tab(int m, int n) {
+    int uniquePaths(int m, int n) {
         if (m == 1 || n == 1) {
             return 1;
         }
         vector<int> a(n);
         for (int i = 0; i < n; i++) {
             a[i] = i + 1;
-        }
-        if (m == 1) {
-            return a[n - 1];
         }
         for (int i = 2; i < m; i++) {
             vector<int> temp(n);
@@ -21,5 +18,4 @@ public:
         }
         return a[n - 1];
     }
-    int uniquePaths(int m, int n) { return tab(m, n); }
 };

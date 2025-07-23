71ac5932947577f7405e1ba7a379756290393330
Udit Gupta <135364043+Uditgupta08@users.noreply.github.com>
2025-07-24 01:13:29 +0530

    Time: 0 ms (100%), Space: 12.9 MB (38.11%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,26 +4,21 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         stack<int> st;
-        vector<int> ans(n2);
-        vector<int> res(n1);
+        unordered_map<int, int> nextGreater;
+        vector<int> res;
         for (int i = n2 - 1; i >= 0; i--) {
             while (!st.empty() && nums2[i] > st.top()) {
                 st.pop();
             }
             if (st.empty()) {
-                ans[i] = -1;
+                nextGreater[nums2[i]] = -1;
             } else {
-                ans[i] = st.top();
+                nextGreater[nums2[i]] = st.top();
             }
             st.push(nums2[i]);
         }
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < n2; j++) {
-                if (nums1[i] == nums2[j]) {
-                    res[i] = ans[j];
-                    break;
-                }
-            }
+        for (int it : nums1) {
+            res.push_back(nextGreater[it]);
         }
         return res;
     }

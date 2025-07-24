592361972c1a45d2051cb106e95f6fd6a7d402b5
Udit Gupta <135364043+Uditgupta08@users.noreply.github.com>
2025-07-25 01:08:08 +0530

    Time: 16 ms (86.41%), Space: 139.1 MB (60.34%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,14 +5,21 @@ public:
         deque<int> dq;
         int n = nums.size();
         for (int i = 0; i < n; i++) {
+
+            // if front is less than start of currect window then pop
             if (!dq.empty() && dq.front() <= i - k) {
                 dq.pop_front();
             }
+
+            // if curr ele > ele at back of dq then that ele(in dq) will not be used in next windows
             while (!dq.empty() && nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
+            
+            // just push curr ele(we may use it in next window)
             dq.push_back(i);
 
+            // just tell the answer for each subarray after kth index
             if (i >= k - 1) {
                 ans.push_back(nums[dq.front()]);
             }

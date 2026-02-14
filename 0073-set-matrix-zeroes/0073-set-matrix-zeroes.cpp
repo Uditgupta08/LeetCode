094dc4ebcc8391959aad900f6a03a275b43ba19f
Udit Gupta <135364043+Uditgupta08@users.noreply.github.com>
2026-02-14 23:22:33 +0530

    Time: 1 ms (14.9%), Space: 18.6 MB (79.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,55 +1,54 @@
 class Solution {
 public:
+    // Basic Idea:
+    /*
+        Check if first row or column contains 0 -> Check bools
+    */
     void setZeroes(vector<vector<int>>& a) {
-        bool r = false;
-        bool c = false;
         int n = a.size();
         int m = a[0].size();
-
-        for (int i = 0; i < m; i++) {
-            if (a[0][i] == 0) {
-                r = true;
-                break;
+        bool lc = false;
+        bool tr = false;
+        for(int i = 0; i < n; i++){
+            if(a[i][0] == 0){
+                lc = true;
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (a[i][0] == 0) {
-                c = true;
-                break;
+        for(int i = 0; i < m; i++){
+            if(a[0][i] == 0){
+                tr = true;
             }
         }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (a[i][j] == 0) {
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(a[i][j] == 0){
                     a[i][0] = 0;
                     a[0][j] = 0;
                 }
             }
         }
-        for (int i = 1; i < n; i++) {
-            if (a[i][0] == 0) {
-                for (int j = 1; j < m; j++) {
+        for(int j = 1; j < m; j++){
+            if(a[0][j] == 0){
+                for(int i = 1; i < n; i++){
                     a[i][j] = 0;
                 }
             }
         }
-        for (int i = 1; i < m; i++) {
-            if (a[0][i] == 0) {
-                for (int j = 1; j < n; j++) {
-                    a[j][i] = 0;
+        for(int i = 1; i < n; i++){
+            if(a[i][0] == 0){
+                for(int j = 1; j < m; j++){
+                    a[i][j] = 0;
                 }
             }
         }
-        if (r) {
-            for (int i = 0; i < m; i++) {
+        if(tr){
+            for(int i = 0; i < m; i++){
                 a[0][i] = 0;
             }
         }
-        if (c) {
-            for (int i = 0; i < n; i++) {
-                a[i][0] = 0;
+        if(lc){
+            for(int j = 0; j < n; j++){
+                a[j][0] = 0;
             }
         }
     }
